496c3e4f70eaca54613e90844756edf2232af4cc
rup-ff <86917835+rup-ff@users.noreply.github.com>
2021-11-20 10:59:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ if(error)
   document.getElementById("result_emotion_name").innerHTML = results[0].label;
   document.getElementById("result_emotion_name2").innerHTML = results[1].label;
 prediction_1 = results[0].label;
-prediction_2 = rsluts[1].label;
+prediction_2 = resluts[1].label;
 speak();
 if(results[0].label == "HAPPY")
 {
